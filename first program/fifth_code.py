e545d4629edd1865dee6474935ca4b87d7701b83
Evhenii <whiletheyoung@gmail.com>
2021-11-14 22:49:36 +0200

    first program\fifth_code.py first program\sixth_code.py first program\seventh_code.py first program\example.py Cheetsheets\Functions and Conditionals.txt
    Signed-off-by: Evhenii <whiletheyoung@gmail.com>

--- first program/fifth_code.py
+++ first program/fifth_code.py
@@ -1,2 +1,7 @@
-monday_temperatures = (1, 4, 5)
-print(monday_temperatures)
+def mean(myList): 
+    print("Function started!")
+    the_mean = sum(myList) / len(myList)
+    return the_mean
+
+mymean = mean([0, 3, 4])
+print(mymean)
