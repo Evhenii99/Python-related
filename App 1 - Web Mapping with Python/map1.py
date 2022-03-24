197a72e0282b06852b75af3d1f6433ff2c10eca7
Evhenii <whiletheyoung@gmail.com>
2022-03-24 21:15:28 +0200

    App 1 - Web Mapping with Python. Update codding (2022.03.24)

--- App 1 - Web Mapping with Python/map1.py
+++ App 1 - Web Mapping with Python/map1.py
@@ -1,8 +1,30 @@
 import folium
-map = folium.Map(location=[50.404536445610404, 30.33755341976802], zoom_start=6, tiles="Stamen Terrain")
+from folium.plugins import MarkerCluster
+import pandas
+
+
+data = pandas.read_csv("Volcanoes.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation <3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map = folium.Map(location=[44.05423792776983, -121.318977164173], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[50.3, 30.4], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+ " m",
+    fill_color=color_producer(el), color = 'gray', fill_opacity=0.7))
+
 map.add_child(fg)
 
 map.save("Map1.html")
