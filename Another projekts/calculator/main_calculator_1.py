6614c5068af045d7f679523a9f5c08bcaddd8f32
Evhenii <whiletheyoung@gmail.com>
2023-02-06 15:52:32 +0200

    Deleted extra lines Used function in calculator 1

--- Another projekts/calculator/main_calculator_1.py
+++ Another projekts/calculator/main_calculator_1.py
@@ -1,58 +1,37 @@
-new = True
+run = True
 
+while run:
+    what_todo = input("What do you want to do?\n1 for +\n2 for -\n3 for *\n4 for /\n: ")
 
-
-
-
-while new:
-    user_actions = input("Choice your number: 1 for +, 2 for -, 3 for *, 4 for /: ")
-    user_actions = user_actions.strip()
-
-    match user_actions:
-        case '1':
-            first_number = int(input("Enter first number: "))
-            second_number = int(input("Enter second number: "))
+    def mathematical_operation():
+        first_number = int(input("Enter first number: "))
+        second_number = int(input("Enter second number: "))
+        if what_todo == '1':
             result = first_number + second_number
-            print(f"Answer is {result}")
-            user_input = input("Do you want to continue? Enter Yes or No: ")
-            user_input = user_input.capitalize()
-            if user_input == 'Yes':
-                new = True
-            else:
-                break
-        case '2':
-            first_number = int(input("Enter first number: "))
-            second_number = int(input("Enter second number: "))
+        elif what_todo == '2':
             result = first_number - second_number
-            print(f"Answer is {result}")
-            user_input = input("Do you want to continue? Enter Yes or No: ")
-            user_input = user_input.capitalize()
-            if user_input == 'Yes':
-                new = True
-            else:
-                break
-        case '3':
-            first_number = int(input("Enter first number: "))
-            second_number = int(input("Enter second number: "))
+        elif what_todo == '3':
             result = first_number * second_number
-            print(f"Answer is {result}")
-            user_input = input("Do you want to continue? Enter Yes or No: ")
-            user_input = user_input.capitalize()
-            if user_input == 'Yes':
-                new = True
-            else:
-                break
-        case '4':
-            first_number = int(input("Enter first number: "))
-            second_number = int(input("Enter second number: "))
+        elif what_todo == '4':
             result = first_number / second_number
-            print(f"Answer is {result}")
-            user_input = input("Do you want to continue? Enter Yes or No: ")
-            user_input = user_input.capitalize()
-            if user_input == 'Yes':
-                new = True
-            else:
-                break
+        print(f"Answer is {result}")
+
+    match what_todo:
+        case '1':
+            mathematical_operation()
+        case '2':
+            mathematical_operation()
+        case '3':
+            mathematical_operation()
+        case '4':
+            mathematical_operation()
         case _:
-            print("Hey, you entered an unknown command")
-print("Have a nice day!")
+            print("Your value in not 1-4")
+
+    user_input = input("Do you want to continue? Enter Yes or No: ")
+    user_input = user_input.capitalize()
+    if user_input == 'Yes':
+        run = True
+    else:
+        break
+print("Thank for using this program, have a nice day!")
