db8765d7033bc64d89e66e869c27de7b5eabb155
Evhenii <whiletheyoung@gmail.com>
2023-02-06 20:18:19 +0200

    Ended section 7 on Udemy lerned list comprehensions

---
+++ Python_new_2023/Section 7/test.py
@@ -0,0 +1,19 @@
+# names = ["john smith", "jay santi", "eva kuki"]
+# new_names = [name.title() for name in names]
+# print(new_names)
+
+# usernames = ["john 1990", "alberta1970", "magnola2000"]
+# usernames = [len(username) for username in usernames]
+# print(usernames)
+
+# user_entries = ['10', '19.1', '20']
+# user_entries = [float(entries) for entries in user_entries]
+# print(user_entries)
+
+# user_entries = ['10', '19.1', '20']
+# user_entries = [float(entries) for entries in user_entries]
+# print(sum(user_entries))
+
+numbers = [10.1, 12.3, 14.7]
+numbers = [int(item) for item in numbers]
+print(numbers)
