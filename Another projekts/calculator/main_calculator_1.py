1004dbd616246a9ab53fadf40e22d42b84f8eb65
Evhenii <whiletheyoung@gmail.com>
2023-02-08 14:22:46 +0200

    i added a parameter to mathematical_operation() And deleted duplicate match case

--- Another projekts/calculator/main_calculator_1.py
+++ Another projekts/calculator/main_calculator_1.py
@@ -1,32 +1,26 @@
 run = True
 
 while run:
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
     what_todo = input("What do you want to do?\n1 for +\n2 for -\n3 for *\n4 for /\n: ")
 
-    def mathematical_operation():
-        first_number = int(input("Enter first number: "))
-        second_number = int(input("Enter second number: "))
+    def mathematical_operation(choise):
         if what_todo == '1':
             result = first_number + second_number
         elif what_todo == '2':
             result = first_number - second_number
+            print(f"The answer is {result}")
         elif what_todo == '3':
             result = first_number * second_number
+            print(f"The answer is {result}")
         elif what_todo == '4':
             result = first_number / second_number
-        print(f"Answer is {result}")
-
-    match what_todo:
-        case '1':
-            mathematical_operation()
-        case '2':
-            mathematical_operation()
-        case '3':
-            mathematical_operation()
-        case '4':
-            mathematical_operation()
-        case _:
+            print(f"The answer is {result}")
+        else:
             print("Your value in not 1-4")
+
+    mathematical_operation(what_todo)
 
     user_input = input("Do you want to continue? Enter Yes or No: ")
     user_input = user_input.capitalize()
